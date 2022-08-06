a514ce4e21794da0425f39f2bdbcb691d40a8d1a
Dhriti Dey <92095533+florencered@users.noreply.github.com>
2022-08-07 02:15:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 console.log("working");  
-let countryNames=[]; //array
+let countryNames=[]; 
+//global variables
 const searchInput = document.getElementById('search');
 const searchWrapper = document.querySelector('.wrapper');
 const resultsWrapper = document.querySelector('.results');
-let countries; //will contain fetched data,simiar to let countries={}
-//event listener for change 
-/*document.getElementById("countries").addEventListener('change',(event)=>{ 
-
-	console.log(event.target.value);
-	displayCountryInfo(event.target.value);
-})*/ 
-
+let countries; 
 /**************fetching data**************/
-fetch("https://restcountries.com/v3.1/all")
-.then(function (response){ 
-	return response.json()
+/*fetch("https://restcountries.com/v3.1/all")
+.then( function (response){ 
+	return  response.json()
 
 })
 
@@ -24,15 +18,23 @@ fetch("https://restcountries.com/v3.1/all")
 }) 
 .catch(function(err){
 	console.log("Error:",err);
-});
-/***************pushing country names from array to api*******************/
+});*/ 
+(async ()=>{
+	try{
+		let fetchData= await fetch("https://restcountries.com/v3.1/all"); 
+		let result=await fetchData.json(); 
+		initialise(result);
+	} 
+	catch(err){
+		console.error(err);
+	}
+})();
+/********pushing country names from array to api********/
 function initialise(countriesData){ 
 	countries=countriesData //variable for all the country names obtained through fetch api
 	console.log(countriesData);
 	let items="" //will add the country names as options in the inner html 
 	countries.forEach(country=>(countryNames.push(country.name.official)) )
-	//document.getElementById('countries').innerHTML=items; 
-	//to show the info of the selected country 
 	
 
 
@@ -46,102 +48,91 @@ let fun = (country) => {
 	document.getElementById("search").value=country;
 }
 
-/************searchbar*********************/
+/************searchbar***************/
 
 searchInput.addEventListener('keyup', () => {
-  let results = [];
-  let input = searchInput.value;
-  if (input.length) {
-    results = countryNames.filter((item) => {
-      return item.toLowerCase().includes(input.toLowerCase()); 
+	let results = [];
+	let input = searchInput.value;
+	if (input.length) {
+		results = countryNames.filter((item) => {
+			return item.toLowerCase().includes(input.toLowerCase()); 
 
-    });
-  }
-  renderResults(results);
+		});
+	}
+	renderResults(results);
 });
 
 function renderResults(results){
 	console.log("Results:",results);
-  if (!results.length) {
-    return searchWrapper.classList.remove('show');
-  }
+	if (!results.length) {
+		return searchWrapper.classList.remove('show');
+	}
 
-  const content = results
-    .map((item) => { 
-      return `<li onclick="fun('${item}')" class="countries">${item}</li>`;
-    })
-    .join(''); 
-    
+	const content = results
+	.map((item) => { 
+		return `<li onclick="fun('${item}')" class="countries">${item}</li>`;
+	})
+	.join(''); 
 
-  searchWrapper.classList.add('show');
-  resultsWrapper.innerHTML = `<ul >${content}</ul>`;
+
+	searchWrapper.classList.add('show');
+	resultsWrapper.innerHTML = `<ul >${content}</ul>`;
 } 
-/******************register input on searchbar************************/ 
+/************register input on searchbar******************/ 
 
-/*document.querySelector("countries").addEventListener('click',(event)=>{ 
-
-	console.log(event.target);
-	//displayCountryInfo(event.target.value);
-}) */
-
-/********************display the content******************/
+/**************display the content******************/
 function displayCountryInfo(countryName){
 	const countryData=countries.find(c=>c.name.official===countryName)
-//.find works similar .forEach  
-console.log(countryData); 
-document.getElementById('flagImg').src=countryData.flags.png;
+	console.log(countryData); 
+	document.getElementById('flagImg').src=countryData.flags.png;
 
 
-var language=Object.values(countryData.languages); 
-console.log(language);
-document.getElementById('location').innerHTML=countryData.latlng[0]+" °N"+" ,"+countryData.latlng[1]+" °E";
-document.getElementById('lang').innerHTML=language.join(", ");
-//document.getElementById('location').innerHTML=countryData. 
-document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1920x1080/?" +countryData.name.official+ "')";
-document.getElementById('capital').innerHTML=countryData.capital; 
-document.getElementById('population').innerHTML=countryData.population; 
-console.log(countryData.currencies)
-console.log(Object.values(countryData.currencies)) 
-var currencies=Object.values(countryData.currencies); 
-var filter=currencies.filter(c=>c.name);
-var map=filter.map(c=>`${c.name} (${c.symbol})`); 
-
-document.getElementById('currencies').innerHTML=map.join(", ")
-//.map(c=>`${c.name}(${c.symbol})`).join(", "); 
-//document.getElementById('dcode').innerHTML= 
-document.getElementById('region').innerHTML= countryData.region;
-document.getElementById('lat').innerHTML= countryData.latlng[0]+" °N";
-document.getElementById('long').innerHTML=countryData.latlng[1]+" °E";
-initMap(countryData.latlng[0],countryData.latlng[1],countryData.name.official) 
+	var language=Object.values(countryData.languages); 
+	console.log(language);
+	document.getElementById('location').innerHTML=countryData.latlng[0]+" °N"+" ,"+countryData.latlng[1]+" °E";
+	document.getElementById('lang').innerHTML=language.join(", ");
+	document.body.style.backgroundImage =
+	"url('https://source.unsplash.com/1920x1080/?" +countryData.name.official+ "')";
+	document.getElementById('capital').innerHTML=countryData.capital; 
+	document.getElementById('population').innerHTML=countryData.population; 
+	console.log(countryData.currencies)
+	console.log(Object.values(countryData.currencies)) 
+	var currencies=Object.values(countryData.currencies); 
+	var filter=currencies.filter(c=>c.name);
+	var map=filter.map(c=>`${c.name} (${c.symbol})`); 
+	document.getElementById('currencies').innerHTML=map.join(", ")
+	document.getElementById('region').innerHTML= countryData.region;
+	document.getElementById('lat').innerHTML= countryData.latlng[0]+" °N";
+	document.getElementById('long').innerHTML=countryData.latlng[1]+" °E";
+	initMap(countryData.latlng[0],countryData.latlng[1],countryData.name.official) 
 
 }  
 /********************************google maps***********************************************************************/ 
-function initMap(lat,lng,name){ 
+function initMap(lat=20,lng=77,name='Republic of India'){ 
 		//map features 
 
-			var options={
-				zoom:10, //zooming length
+		var options={
+				zoom:5, //zooming length
 				center:{lat,lng} //adding directions
 
-		} 
-		//generating new map
-		var map= new google.maps.Map(document.getElementById("map"),options) 
-		//add a new marker 
-		const marker = new google.maps.Marker({
-    position:{lat,lng},
-    map: map,//name of the variable for which the new map was created 
+			} 
+
+			var map= new google.maps.Map(document.getElementById("map"),options) 
+
+			const marker = new google.maps.Marker({
+				position:{lat,lng},
+				map: map,
    icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' //change the icon type
-  }); 
-		//to get info as soon as we click on the symbol 
+}); 
+
 		var infoWindow=new google.maps.InfoWindow({content:`"<h4>'${name}'</h4"`}); //displays content in the h1 font size on click
 		marker.addListener("click",function(){
 			infoWindow.open(map,marker);
 		})
-} 
+	} 
 
 
- 
+
 
 
 
